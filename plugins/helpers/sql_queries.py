3bcf30b68ac335a0cc0ba7a9084f4f8ddbe1a827
MarkHash <makoto.hashimoto06@gmail.com>
2021-12-27 11:10:33 +1100

    update load_dimension/fact.py

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -1,5 +1,6 @@
 class SqlQueries:
     songplay_table_insert = ("""
+        INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
         SELECT
                 md5(events.sessionid || events.start_time) songplay_id,
                 events.start_time, 
@@ -20,23 +21,135 @@ class SqlQueries:
     """)
 
     user_table_insert = ("""
+        INSERT INTO users(user_id, first_name, last_name, gender, level)
         SELECT distinct userid, firstname, lastname, gender, level
         FROM staging_events
         WHERE page='NextSong'
     """)
 
     song_table_insert = ("""
+        INSERT INTO songs(song_id, title, artist_id, year, duration)
         SELECT distinct song_id, title, artist_id, year, duration
         FROM staging_songs
     """)
 
     artist_table_insert = ("""
+        INSERT INTO artists(artist_id, name, location, lattitude, longitude)
         SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
         FROM staging_songs
     """)
 
     time_table_insert = ("""
+        INSERT INTO time(start_time, hour, day, week, month, year, weekday)
         SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
                extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
         FROM songplays
+    """)
+
+    CREATE_ARTISTS_TABLE_SQL = ("""
+    CREATE TABLE IF NOT EXISTS public.artists (
+        artistid varchar(256) NOT NULL,
+        name varchar(256),
+        location varchar(256),
+        lattitude numeric(18,0),
+        longitude numeric(18,0)
+    );""")
+
+    CREATE_SONGPLAYS_TABLE_SQL = ("""
+    CREATE TABLE IF NOT EXISTS public.songplays (
+        playid varchar(32) NOT NULL,
+        start_time timestamp NOT NULL,
+        userid int4 NOT NULL,
+        "level" varchar(256),
+        songid varchar(256),
+        artistid varchar(256),
+        sessionid int4,
+        location varchar(256),
+        user_agent varchar(256),
+        CONSTRAINT songplays_pkey PRIMARY KEY (playid)
+    );""")
+
+    CREATE_SONGS_TABLE_SQL = ("""
+    CREATE TABLE IF NOT EXISTS public.songs (
+        songid varchar(256) NOT NULL,
+        title varchar(256),
+        artistid varchar(256),
+        "year" int4,
+        duration numeric(18,0),
+        CONSTRAINT songs_pkey PRIMARY KEY (songid)
+    );""")
+
+    CREATE_STAGING_EVENTS_TABLE_SQL = ("""
+    CREATE TABLE IF NOT EXISTS public.staging_events (
+        artist varchar(256),
+        auth varchar(256),
+        firstname varchar(256),
+        gender varchar(256),
+        iteminsession int4,
+        lastname varchar(256),
+        length numeric(18,0),
+        "level" varchar(256),
+        location varchar(256),
+        "method" varchar(256),
+        page varchar(256),
+        registration numeric(18,0),
+        sessionid int4,
+        song varchar(256),
+        status int4,
+        ts int8,
+        useragent varchar(256),
+        userid int4
+    );""")
+
+    CREATE_STAGING_SONGS_TABLE_SQL = ("""
+    CREATE TABLE IF NOT EXISTS public.staging_songs (
+        num_songs int4,
+        artist_id varchar(256),
+        artist_name varchar(256),
+        artist_latitude numeric(18,0),
+        artist_longitude numeric(18,0),
+        artist_location varchar(256),
+        song_id varchar(256),
+        title varchar(256),
+        duration numeric(18,0),
+        "year" int4
+    );""")
+
+    CREATE_TIME_TABLE_SQL = ("""
+    CREATE TABLE IF NOT EXISTS public."time" (
+        start_time timestamp NOT NULL,
+        "hour" int4,
+        "day" int4,
+        week int4,
+        "month" varchar(256),
+        "year" int4,
+        weekday varchar(256),
+        CONSTRAINT time_pkey PRIMARY KEY (start_time)
+    );""")
+
+    CREATE_USERS_TABLE_SQL = ("""
+    CREATE TABLE IF NOT EXISTS public.users (
+        userid int4 NOT NULL,
+        first_name varchar(256),
+        last_name varchar(256),
+        gender varchar(256),
+        "level" varchar(256),
+        CONSTRAINT users_pkey PRIMARY KEY (userid)
+    );
+    """)
+
+    EVENTS_COPY_SQL = ("""
+    COPY {table}
+    FROM '{s3_directory}'
+    ACCESS_KEY_ID '{access_key}'
+    SECRET_ACCESS_KEY '{secret_key}'
+    JSON 's3://udacity-dend/log_json_path.json' COMPUPDATE ON REGION 'us-west-2'
+    """)
+
+    SONGS_COPY_SQL = ("""
+    COPY {table}
+    FROM '{s3_directory}'
+    ACCESS_KEY_ID '{access_key}'
+    SECRET_ACCESS_KEY '{secret_key}'
+    JSON 'auto' COMPUPDATE OFF REGION 'us-west-2'
     """)
