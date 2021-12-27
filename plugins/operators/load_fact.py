3454759b3c465c66dc2faa79588d27cf90e7c2fa
MarkHash <makoto.hashimoto06@gmail.com>
2021-12-27 17:45:23 +1100

    removed create-table-sql, added append flag

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -1,7 +1,6 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
-from airflow.contrib.hooks.aws_hook import AwsHook
 from plugins.helpers import sql_queries
 
 class LoadFactOperator(BaseOperator):
@@ -11,20 +10,16 @@ class LoadFactOperator(BaseOperator):
     def __init__(self,
                  redshift_conn_id="",
                  table="",
-                 create_table_sql="",
                  insert_sql="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.table = table
-        self.create_table_sql = create_table_sql
         self.insert_sql = insert_sql
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-
-        redshift.run(self.create_table_sql)
 
         self.log.info("Clearing data from destination Redshift table")
         redshift.run("DELETE FROM {}".format(self.table))
